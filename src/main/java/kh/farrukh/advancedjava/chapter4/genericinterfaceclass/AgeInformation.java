dd21323237ddeed04bc7ffdb899b0a41817957fd
f_khusainov <farrukhbekkhusainov@gmail.com>
2024-07-01 00:06:28 +0500

    Added generic methods

--- src/main/java/kh/farrukh/advancedjava/chapter4/genericinterfaceclass/AgeInformation.java
+++ src/main/java/kh/farrukh/advancedjava/chapter4/genericinterfaceclass/AgeInformation.java
@@ -1,7 +1,9 @@
 package kh.farrukh.advancedjava.chapter4.genericinterfaceclass;
 
 import java.time.LocalDate;
+import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAccessor;
+import java.util.function.Function;
 
 public class AgeInformation<T extends TemporalAccessor> implements Informative<T, String> {
 
@@ -9,6 +11,22 @@ public class AgeInformation<T extends TemporalAccessor> implements Informative<T
   public String getInfo(T birthDate) {
     var age = LocalDate.now().getYear() - LocalDate.from(birthDate).getYear();
     return "Age is " + age;
+  }
+
+  /*
+   If methods are declared (or defined) as part of generic interface or class, they may (or may not)
+   use the generic types of their owner.
+   They may define own generic types or mix them with the ones from their class or interface declaration.
+   */
+  public <U> U map(T temporal, Function<T, U> mapper) {
+    return mapper.apply(temporal);
+  }
+
+  /*
+   There are no restrictions on which methods can use generic types, they could be concrete, abstract, static or final
+   */
+  public static <U extends Temporal, V> V mapStatic(U temporal, Function<U, V> mapper) {
+    return mapper.apply(temporal);
   }
 
 }
--- src/main/java/kh/farrukh/advancedjava/chapter4/genericinterfaceclass/GenericInterfaceClassApp.java
+++ src/main/java/kh/farrukh/advancedjava/chapter4/genericinterfaceclass/GenericInterfaceClassApp.java
@@ -6,15 +6,25 @@ import java.time.LocalDateTime;
 public class GenericInterfaceClassApp {
 
   public static void main(String[] args) {
-    Informative<LocalDate, String> ageInformation1 = new AgeInformation<>();
+    AgeInformation<LocalDate> ageInformation1 = new AgeInformation<>();
     var birthDate1 = LocalDate.of(1990, 1, 1);
     var info1 = ageInformation1.getInfo(birthDate1);
     System.out.println(info1);
 
-    Informative<LocalDateTime, String> ageInformation2 = new AgeInformation<>();
+    AgeInformation<LocalDateTime> ageInformation2 = new AgeInformation<>();
     var birthDate2 = LocalDateTime.of(1990, 1, 1, 9, 10);
     var info2 = ageInformation2.getInfo(birthDate2);
     System.out.println(info2);
+
+    var isLeapYear1 = ageInformation1.map(birthDate1, LocalDate::isLeapYear);
+    System.out.println(isLeapYear1);
+    var isLeapYear1Static = AgeInformation.mapStatic(birthDate1, LocalDate::isLeapYear);
+    System.out.println(isLeapYear1Static);
+
+    var birthDate2LocalDate = ageInformation2.map(birthDate2, LocalDateTime::toLocalDate);
+    System.out.println(birthDate2LocalDate);
+    var birthDate2LocalDateStatic = AgeInformation.mapStatic(birthDate2, LocalDateTime::toLocalDate);
+    System.out.println(birthDate2LocalDateStatic);
   }
 
 }
