9e3333d2c8e06880b7b7c0127184f900cb0fa1c6
f_khusainov <farrukhbekkhusainov@gmail.com>
2024-06-30 23:54:00 +0500

    Added generic class (with some bounded type)

--- src/main/java/kh/farrukh/advancedjava/chapter4/genericinterface/AgeInformation.java
+++ src/main/java/kh/farrukh/advancedjava/chapter4/genericinterface/AgeInformation.java
@@ -1,12 +1,13 @@
 package kh.farrukh.advancedjava.chapter4.genericinterface;
 
 import java.time.LocalDate;
+import java.time.temporal.TemporalAccessor;
 
-public class AgeInformation implements Informative<LocalDate, String> {
+public class AgeInformation<T extends TemporalAccessor> implements Informative<T, String> {
 
   @Override
-  public String getInfo(LocalDate birthDate) {
-    var age = LocalDate.now().getYear() - birthDate.getYear();
+  public String getInfo(T birthDate) {
+    var age = LocalDate.now().getYear() - LocalDate.from(birthDate).getYear();
     return "Age is " + age;
   }
 
--- src/main/java/kh/farrukh/advancedjava/chapter4/genericinterface/GenericInterfaceApp.java
+++ src/main/java/kh/farrukh/advancedjava/chapter4/genericinterface/GenericInterfaceApp.java
@@ -1,14 +1,20 @@
 package kh.farrukh.advancedjava.chapter4.genericinterface;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class GenericInterfaceApp {
 
   public static void main(String[] args) {
-    Informative<LocalDate, String> ageInformation = new AgeInformation();
-    var birthDate = LocalDate.of(1990, 1, 1);
-    var info = ageInformation.getInfo(birthDate);
-    System.out.println(info);
+    Informative<LocalDate, String> ageInformation1 = new AgeInformation<>();
+    var birthDate1 = LocalDate.of(1990, 1, 1);
+    var info1 = ageInformation1.getInfo(birthDate1);
+    System.out.println(info1);
+
+    Informative<LocalDateTime, String> ageInformation2 = new AgeInformation<>();
+    var birthDate2 = LocalDateTime.of(1990, 1, 1, 9, 10);
+    var info2 = ageInformation2.getInfo(birthDate2);
+    System.out.println(info2);
   }
 
 }
