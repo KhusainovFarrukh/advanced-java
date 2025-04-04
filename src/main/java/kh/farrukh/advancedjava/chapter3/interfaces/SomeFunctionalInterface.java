6f49e5b9de7507fb969a17f8a24061f555a18422
f_khusainov <farrukhbekkhusainov@gmail.com>
2025-04-04 14:59:41 +0500

    Added FunctionalInterface annotation

--- src/main/java/kh/farrukh/advancedjava/chapter3/interfaces/SomeFunctionalInterface.java
+++ src/main/java/kh/farrukh/advancedjava/chapter3/interfaces/SomeFunctionalInterface.java
@@ -1,7 +1,21 @@
 package kh.farrukh.advancedjava.chapter3.interfaces;
 
+@FunctionalInterface
 public interface SomeFunctionalInterface {
 
+  //but does not give error for constant fields
+  String SOME_CONSTANT = "Some constant";
+  Integer SOME_OTHER_CONSTANT = 10;
+
   void printSomething();
+
+  //FunctionalInterface annotation gives error if the interface has more than one abstract method:
+  //Multiple non-overriding abstract methods found in interface kh.farrukh.advancedjava.chapter3.interfaces.SomeFunctionalInterface
+//  void printSomethingElse();
+
+  //also does not give error for static methods
+  static void printStatic() {
+    System.out.println("Static method in functional interface");
+  }
 
 }
